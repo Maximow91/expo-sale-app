afe715680445e313731a73c56ba1d684a8d1f700
kiryl_maximow <markizzzzka91@gmail.com>
2023-11-16 14:23:44 +0300

    safeArea added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 import {StatusBar} from 'expo-status-bar'
 import {StyleSheet, Text, View} from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Hello!</Text>
+        <StatusBar style='auto' />
+      </View>
+    </SafeAreaProvider>
   )
 }
 
